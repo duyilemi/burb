6614578ea0d2f7c19100a60089c1aae986b7aaf7
duyilemi <67197664+duyilemi@users.noreply.github.com>
2022-03-04 16:40:38 +0100

    update

--- src/axiosMethods.js
+++ src/axiosMethods.js
@@ -8,7 +8,7 @@ const BASE_URL = "https://burbuyit.herokuapp.com/api";
 const DEFAULTTOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjBmNzM0NmVmOWFiZjY5MDRkNzdiNmUiLCJ1c2VybmFtZSI6Imx1bmEiLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNjQ2NDA3MDcyLCJleHAiOjE2NDg5OTkwNzJ9.qWs3eKjsJ5uRgQD7V4Q-lQTj3XP_N7z-zvGSjcYgtLI";
 
-let TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+let TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
   .currentUser?.token;
 
 TOKEN ??= DEFAULTTOKEN;
